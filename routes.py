d9cf0370945cb197c0a64bb49ffac1d16b1e56e0
paaulorickardo <paaulorickardo@hotmail.com>
2021-12-27 11:19:06 -0300

    geraResponse ok

--- routes.py
+++ routes.py
@@ -12,15 +12,14 @@ def olaMundo():
 
 @app.route("/cadastrar/usuario", methods=["POST"])
 def cadastraUsuario():
-
     body = request.get_json()
 
-    if("nome" not in body):
-        return {"status": 400,"mensagem": "O parametro é obrigatorio"}
+    if ("nome" not in body):
+        return {"status": 400, "mensagem": "O parametro é obrigatorio"}
 
     usuario = insertUsuario(body["nome"], body["email"], body["senha"])
 
-    return geraResponse(200, "Usúrio criado com sucesso")
+    return geraResponse(200, "Usuário criado com sucesso", "user", usuario)
 
 
 def geraResponse(status, mensagem, nome_do_conteudo=False, conteudo=False):
@@ -28,7 +27,9 @@ def geraResponse(status, mensagem, nome_do_conteudo=False, conteudo=False):
     response["status"] = status
     response["mensagem"] = mensagem
 
-    if(nome_do_conteudo and conteudo):
+    if (nome_do_conteudo and conteudo):
         response[nome_do_conteudo] = conteudo
+        return response
+
 
 app.run()
